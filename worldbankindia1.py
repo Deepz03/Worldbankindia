3de44978108b815aa5abbfa88014e3ee7fe2e032
Deepan Raj <88298712+Deepz03@users.noreply.github.com>
2022-04-17 16:39:40 +0530

    Add files via upload

---
+++ worldbankindia1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+import streamlit as st
+
+#printing the title of the webapp
+st.title('World Bank Data - India')
+
+#importing the csv file
+India=pd.read_csv("World_Bank_India2.csv")
+India.set_index('years',inplace=True)
+India.T
